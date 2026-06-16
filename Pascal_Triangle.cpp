9d740c3c8271e67c2064bd2fb3d52a03be553bb8
Akmal-ui <akmalmalik6899@gmail.com>
2026-06-16 14:34:54 +0530

    solved pascal triangle

---
+++ Pascal_Triangle.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1);
+
+            for (int j = 1; j < i; j++) {
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+
+            triangle.push_back(row);
+        }
+
+        return triangle;
+    }
+};
+
+int main() {
+    Solution sol;
+    int numRows;
+
+    cout << "Enter number of rows: ";
+    cin >> numRows;
+
+    vector<vector<int>> result = sol.generate(numRows);
+
+    cout << "\nPascal's Triangle:\n";
+    for (const auto& row : result) {
+        for (int num : row) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
