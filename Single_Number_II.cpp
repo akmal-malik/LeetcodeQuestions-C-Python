494f8812dba66bf0df631434e4bbbaf9526504bf
Akmal-ui <akmalmalik6899@gmail.com>
2026-06-16 15:03:41 +0530

    solved Single Number problem part 2

---
+++ Single_Number_II.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (auto it : freq) {
+            if (it.second == 1) {
+                return it.first;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 2, 3, 2};
+
+    Solution obj;
+    int ans = obj.singleNumber(nums);
+
+    cout << "Single Number: " << ans << endl;
+
+    return 0;
+}
