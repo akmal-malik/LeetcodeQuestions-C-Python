b6722b1625739869b367cc6b0e9b92888f8e28b0
Akmal-ui <akmalmalik6899@gmail.com>
2026-06-18 09:20:25 +0530

    updated

--- python/lamba.py
+++ python/lamba.py
@@ -1,2 +1,5 @@
 add = lambda a,b : a*b
 print(add(398,33))
+
+# lamba is an anonymous function that can take any number of arguments but can only have one expression. It is often used for short, simple functions that are not reused elsewhere in the code.
+# In this example, we define a lambda function that multiplies two numbers and then call it with the arguments 398 and 33, printing the result.
